fefece5cb4b338482737b8582e20e4b5e086bdd4
DachiSkhiladze <dachiskhiladze@gmail.com>
2023-05-14 23:37:04 +0400

    Initial Commit Of Microservices

---
+++ SM-POST/Post.Cmd/Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,36 @@
+using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Post.Cmd.Infrastructure.Config;
+
+namespace Post.Cmd.Infrastructure.Repositories
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        private readonly IMongoCollection<EventModel> _eventStoreCollecion;
+
+        public EventStoreRepository(IOptions<MongoDbConfig> config)
+        {
+            var mongoClient = new MongoClient(config.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(config.Value.Database);
+
+            _eventStoreCollecion = mongoDatabase.GetCollection<EventModel>(config.Value.Collection);
+        }
+
+        public async Task<List<EventModel>> FindAllAsync()
+        {
+            return await _eventStoreCollecion.Find(_ => true).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
+        {
+            return await _eventStoreCollecion.Find(x => x.AggregateIdentifier == aggregateId).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task SaveAsync(EventModel @event)
+        {
+            await _eventStoreCollecion.InsertOneAsync(@event).ConfigureAwait(false);
+        }
+    }
+}
